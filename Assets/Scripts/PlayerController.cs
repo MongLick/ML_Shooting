bdc9ba611764a2ff9c602951a81cc02dac3dbe29
MongLick <qkjm4836@gmail.com>
2024-07-01 19:35:51 +0900

    feat : 캐릭터 컨트롤러, FPS, TPS 이동 & 시점

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Component")]
+    [SerializeField] CharacterController controller;
+
+    [Header("Spec")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpSpeed;
+
+    private Vector3 moveDir;
+    private float ySpeed;
+
+	private void Update()
+	{
+		Move();
+        JumpMove();
+	}
+
+	private void OnMove(InputValue value)
+    {
+        Vector2 inputDir = value.Get<Vector2>();
+        moveDir.x = inputDir.x;
+        moveDir.z = inputDir.y;
+    }
+
+    private void OnJump(InputValue value)
+    {
+        if(controller.isGrounded)
+        {
+			ySpeed = jumpSpeed;
+		}
+    }
+
+    private void Move()
+    {
+        // 이건 보고 있는 방향이 아님
+        // controller.Move(moveDir * moveSpeed * Time.deltaTime);
+
+        controller.Move(transform.right * moveDir.x * moveSpeed * Time.deltaTime);
+		controller.Move(transform.forward * moveDir.z * moveSpeed * Time.deltaTime);
+	}
+
+    private void JumpMove()
+    {
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if(controller.isGrounded)
+        {
+            ySpeed = 0;
+        }
+
+        controller.Move(Vector3.up * ySpeed * Time.deltaTime);
+    }
+}
